054c44585da0bd16c5f167bb90a19d76e0158ba6
crywolfe <gwolfe2007@gmail.com>
2014-09-01 14:43:24 -0500

    simple express app using EJS

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+// var connect = require('connect');
+// var auth = require('basic-auth');
+// var user = auth({name: "me", pass: "123"});
+
 var express = require('express'),
   port = 3000;
 
 var app = express();
 
+
+// middleware is added using the use function
+// app.use(function(req, res, next){
+//   console.log('hi from middleware');
+//   next();
+// });
+
+
+// app.use(function(req, res, next){
+//   console.log('doing async stuff...');
+//   setTimeout(function(){
+//     console.log('done');
+//     next();
+//   }, 1000);
+// });
+app.set('view engine', 'ejs');
+
+app.get('/:name', function(req, res){
+  // res.send("hello, " + req.params.name);
+  res.render('greeting.ejs', {name: req.params.name});
+});
+
 app.get('/', function(req, res){
   res.send('get request');
 });
-app.get('/:name', function(req, res){
-  res.send("hello, " + req.params.name);
-});
+
 app.post('/', function(req, res){
   res.send('post request');
 });
